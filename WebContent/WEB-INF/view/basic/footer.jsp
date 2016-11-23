664161576c760d49a8265cd93ea50ade8cae1d51
admin <admin@192.168.10.12>
2016-11-23 15:21:31 +0900

    현찬이형 11/23 3:21

--- WebContent/WEB-INF/view/basic/footer.jsp
+++ WebContent/WEB-INF/view/basic/footer.jsp
@@ -1,13 +1,58 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+   pageEncoding="UTF-8"%>
 <%
-	String addr = request.getLocalAddr();
+   String addr = request.getLocalAddr();
 %>
+<script
+   src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="/script/jquery.cookie.js"></script>
+<audio preload="auto" controls="controls" src="/audio/p.mp3" loop=""
+   style="width: 100%; height: 15px;" > 
+</audio>
+<script>
 
-<div align="center">
-<!-- mp4,ogg webm -->
-<video controls="controls" width="100%" height="15">
-<source src="/audio/ttm.mp4">
-</video>
+function setCookie(c_name,value,exdays)
+{
+    $.cookie(c_name, escape(value), {
+        path: '/'
+    }); 
+}
 
-</div>
+function getCookie(c_name)
+{
+    var i,x,y,ARRcookies=document.cookie.split(";");
+    for (i=0;i<ARRcookies.length;i++)
+    {
+       console.log(ARRcookies[i]);
+         x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+         y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+         x=x.replace(/^\s+|\s+$/g,"");
+         if (x==c_name)
+        {
+        return unescape(y);
+        }
+      }
+}
+
+var song = document.getElementsByTagName('audio')[0];
+var played = false;
+var tillPlayed = getCookie('timePlayed');
+function update()
+{
+    if(!played){
+        if(tillPlayed){
+        song.currentTime = tillPlayed;
+        song.play();
+        played = true;
+        }
+        else {
+                song.play();
+                played = true;
+        }
+    }
+    else {
+    setCookie('timePlayed', song.currentTime);
+    }
+}
+setInterval(update,1000);
+</script>
--- src/board/controller/BoardController.java
+++ src/board/controller/BoardController.java
@@ -17,91 +17,90 @@ import board.model.Review;
 import board.model.ReviewService;
 
 @Controller
-@RequestMapping("/board")
 public class BoardController {
-	@Autowired
-	BoardService bs;
-	@Autowired
-	ReviewService rs;
-	
-	@RequestMapping("/list")
-	public ModelAndView list(@RequestParam(defaultValue="1")int p) {
-		ModelAndView mav = new ModelAndView();
-		List li = bs.readSomePage(p);
-		mav.addObject("data", li);
-		System.out.println(li);
-		mav.addObject("p",p);
-		System.out.println("p="+p);
-		mav.addObject("last",bs.calcLast());
-		mav.addObject("size",bs.getTotalCount());
-		mav.setViewName("tm:board/list");
-		return mav;
-	}
+   @Autowired
+   BoardService bs;
+   @Autowired
+   ReviewService rs;
+   
+   @RequestMapping("/board/list")
+   public ModelAndView list(@RequestParam(defaultValue="1")int p) {
+      ModelAndView mav = new ModelAndView();
+      List li = bs.readSomePage(p);
+      mav.addObject("data", li);
+      System.out.println(li);
+      mav.addObject("p",p);
+      System.out.println("p="+p);
+      mav.addObject("last",bs.calcLast());
+      mav.addObject("size",bs.getTotalCount());
+      mav.setViewName("tm:board/list");
+      return mav;
+   }
 
-	@RequestMapping("/register")
-	   public ModelAndView register(Board b) {
-	      System.out.println(b.getSubject()+" / "+ b.getComments()+" / "+b.getNum());
-	      ModelAndView mav = new ModelAndView();
-	      if(b!=null){
-	    	  mav.addObject("board", b);
-	      }
-	      mav.setViewName("tm:board/register");
-	      return mav;
-	   }
+   @RequestMapping("/board/register")
+      public ModelAndView register(Board b) {
+         System.out.println(b.getSubject()+" / "+ b.getComments()+" / "+b.getNum());
+         ModelAndView mav = new ModelAndView();
+         if(b!=null){
+            mav.addObject("board", b);
+         }
+         mav.setViewName("tm:board/register");
+         return mav;
+      }
 
-	@RequestMapping("/view")
-	public ModelAndView view(int num) {
-		ModelAndView mav = new ModelAndView();
-		Board b = bs.selectNum(num);
-		List<Review> lr = rs.getReview(num);
-		mav.addObject("board", b);
-		mav.addObject("review", lr);
-		mav.setViewName("tm:board/view");
-		return mav;
-	}
+   @RequestMapping("/board/view")
+   public ModelAndView view(int num) {
+      ModelAndView mav = new ModelAndView();
+      Board b = bs.selectNum(num);
+      List<Review> lr = rs.getReview(num);
+      mav.addObject("board", b);
+      mav.addObject("review", lr);
+      mav.setViewName("tm:board/view");
+      return mav;
+   }
 
-	@RequestMapping(value = "/insertin")
-	public ModelAndView insertVisit(Board vb, HttpSession session) {
-		ModelAndView mav = new ModelAndView();
-		String id = (String) session.getAttribute("userId");
+   @RequestMapping(value = "/board/insertin")
+   public ModelAndView insertVisit(Board vb, HttpSession session) {
+      ModelAndView mav = new ModelAndView();
+      String id = (String) session.getAttribute("userId");
 
-		System.out.println(vb.getComments() + "/" + vb.getLikes() + "/" + vb.getNum() + "/" + vb.getSubject() + "/"
-				+ vb.getViewcount() + "/" + vb.getWriter()+ "/" + vb.getType());
-		boolean flag = bs.insert(id, vb);
-		if (flag) {
-			mav.setViewName("board/success");
-			System.out.println("성공2");
-		} else {
-			mav.setViewName("err");
-			System.out.println("실패");
-		}
-		return mav;
-	}
-	
-	@RequestMapping("/update")
-	public ModelAndView updateBoard(Board b, HttpSession session){
-		ModelAndView mav = new ModelAndView();
-		String writer = (String) session.getAttribute("userId");
-		b.setWriter(writer);
-		System.out.println(b);
-		System.out.println("update: "+b.getComments() + "/" + b.getLikes() + "/" + b.getNum() + "/" + b.getSubject() + "/"
-				+ b.getViewcount() + "/" + b.getWriter());
-		boolean flag = bs.update(b);
-		System.out.println(flag);
-		mav.setViewName("board/success");
-		return mav;
-	}
-	// 게시판 수정
-	@RequestMapping("/delete")
-	@ResponseBody
-	public String deleteBoard(int num){
-		boolean flag = bs.delete(num);
-		System.out.println(flag);
-		if(flag){
-			return "TRUE";
-		}else{
-			return "FALSE";
-		}
-		
-	}
+      System.out.println(vb.getComments() + "/" + vb.getLikes() + "/" + vb.getNum() + "/" + vb.getSubject() + "/"
+            + vb.getViewcount() + "/" + vb.getWriter()+ "/" + vb.getType());
+      boolean flag = bs.insert(id, vb);
+      if (flag) {
+         mav.setViewName("board/success");
+         System.out.println("성공2");
+      } else {
+         mav.setViewName("err");
+         System.out.println("실패");
+      }
+      return mav;
+   }
+   
+   @RequestMapping("/board/update")
+   public ModelAndView updateBoard(Board b, HttpSession session){
+      ModelAndView mav = new ModelAndView();
+      String writer = (String) session.getAttribute("userId");
+      b.setWriter(writer);
+      System.out.println(b);
+      System.out.println("update: "+b.getComments() + "/" + b.getLikes() + "/" + b.getNum() + "/" + b.getSubject() + "/"
+            + b.getViewcount() + "/" + b.getWriter());
+      boolean flag = bs.update(b);
+      System.out.println(flag);
+      mav.setViewName("board/success");
+      return mav;
+   }
+   // 게시판 수정
+   @RequestMapping("/board/delete")
+   @ResponseBody
+   public String deleteBoard(int num){
+      boolean flag = bs.delete(num);
+      System.out.println(flag);
+      if(flag){
+         return "TRUE";
+      }else{
+         return "FALSE";
+      }
+      
+   }
 }
